be8b326f95113e64e9d5b6d5dade1f5d35c70810
pramisha <pramishaprami2@gmail.com>
2025-07-04 14:32:26 +0530

    Location.java

--- src/com/vetias/java/temperaturedata/beans/Location.java
+++ src/com/vetias/java/temperaturedata/beans/Location.java
@@ -16,4 +16,60 @@ public class Location {
     public String getName() {
         return name;
     }  
-}     
+    
+    public void setname(double latitude) {
+        latitude = latitude;
+    }
+    
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitutde() {
+        return  longituted;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    
+    public String getPostalCode() {
+        return postalcode;
+    }
+
+    public String getcountry() {
+        return country;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public void setLongitude(double longitude) {
+        this.longituted = longitude;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setPostalCode(String postalCode) {
+        this.postalcode = postalCode;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {      
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longituted +
+                ", address='" + address + '\'' +
+                ", postalCode='" + postalcode + '\'' +
+                ", contact='" + country + '\'' +
+                '}';
+    }
+    
+}
