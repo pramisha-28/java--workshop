c80d9112bd2bdbaa066d7a17729e8019854e5f3c
pramisha <pramishaprami2@gmail.com>
2025-07-07 14:22:12 +0530

    StringBuilder.java

---
+++ basics/array/StringBuilderExample.java
@@ -0,0 +1,26 @@
+package basics.array;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb  = new StringBuilder();
+        sb.append ("java");
+        sb.append(", ").append("is");
+        sb.append(", ").append("the");
+        sb.append(", ").append("most");
+        sb.append(", ").append("used");
+        sb.append(", ").append("language");
+        sb.append(", ").append("for");
+        sb.append(", ").append("Enterprise");
+        sb.append(", ").append("Application");
+        System.out.println(sb.toString());
+
+
+
+
+
+
+
+
+    }
+    
+}
