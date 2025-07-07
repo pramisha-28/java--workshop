4fdbe28bf6bfc9c104d9534f8fd815f42a04acea
pramisha <pramishaprami2@gmail.com>
2025-07-07 10:41:42 +0530

    Example.java

--- src/com/vetias/java/temperaturedata/beans/StringExample.java
+++ src/com/vetias/java/temperaturedata/beans/StringExample.java
@@ -6,10 +6,13 @@ public class StringExample {
         String lastName = new String("dhanush");
         String ShortName = "prami";
         String buddyName = new String("pramisha");
-        System.out.println(FirstName);
-        String fullName = FirstName+""+lastName;
-        String finalName = FirstName.concat(lastName);
-        System.out.println(fullName);
+        System.out.println(FirstName == ShortName);
+        // String fullName = FirstName+""+lastName;
+        // String finalName = FirstName.concat(lastName);
+        // System.out.println(finalName.toUpperCase());
+        String sentence = "This program shows how can we split a string into multiple strings";
+        String Words[] = sentence.split("");
+        System.out.println(Words.length);
     }
 }   
         
