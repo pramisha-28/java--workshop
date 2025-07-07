29c5f1149e0ba61bf7f7859ac358929416eae634
pramisha <pramishaprami2@gmail.com>
2025-07-07 10:21:58 +0530

    StringExample.java

--- src/com/vetias/java/temperaturedata/beans/Location.java
+++ src/com/vetias/java/temperaturedata/beans/Location.java
@@ -71,5 +71,7 @@ public class Location {
                 ", contact='" + country + '\'' +
                 '}';
     }
+}               
     
-}
+
+    
---
+++ src/com/vetias/java/temperaturedata/beans/StringExample.java
@@ -0,0 +1,18 @@
+package src.com.vetias.java.temperaturedata.beans;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String FirstName = "pramisha";
+        String lastName = new String("dhanush");
+        String ShortName = "prami";
+        String buddyName = new String("pramisha");
+        System.out.println(FirstName);
+        String fullName = FirstName+""+lastName;
+        String finalName = FirstName.concat(lastName);
+        System.out.println(fullName);
+    }
+}   
+        
+    
+    
+
