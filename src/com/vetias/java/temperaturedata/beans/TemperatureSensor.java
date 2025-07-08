05f4e3dc5110569fa7c14c1fe434f381ed8f622c
pramisha <pramishaprami2@gmail.com>
2025-07-08 15:43:16 +0530

    Calculator.java

--- src/com/vetias/java/temperaturedata/beans/TemperatureSensor.java
+++ src/com/vetias/java/temperaturedata/beans/TemperatureSensor.java
@@ -1,6 +1,6 @@
 package src.com.vetias.java.temperaturedata.beans;
 
-public class TemperatureSensor implements Sensor {
+public class TemperatureSensor{
     private float temperature;
     public TemperatureSensor (float aTemperature) {
         temperature =aTemperature;
