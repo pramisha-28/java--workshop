98f8a39eb4b207239b7b055e4d082578e58d4ca6
pramisha <pramishaprami2@gmail.com>
2025-07-09 11:07:11 +0530

    logo.png

--- src/com/vetias/java/workshop/Standardbild/FileCopyUsingBinary.java
+++ src/com/vetias/java/workshop/Standardbild/FileCopyUsingBinary.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 
 public class FileCopyUsingBinary {
     public static void main(String[] args) {
-        try(FileInputStream inputStream = new FileInputStream(".jpeg");
-            FileOutputStream outputStream = new FileOutputStream(".jpeg"))
+        try(FileInputStream inputStream = new FileInputStream("logo.png");
+            FileOutputStream outputStream = new FileOutputStream("pramisha.png"))
         {
             byte[] chunk = new byte[1024];
             int byteRead;
