6338395cd31ed21330561f25c5ff442f822f6fce
pramisha <pramishaprami2@gmail.com>
2025-07-09 11:00:40 +0530

    new

---
+++ src/com/vetias/java/workshop/Standardbild/FileCopyUsingBinary.java
@@ -0,0 +1,22 @@
+package src.com.vetias.java.workshop.Standardbild;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyUsingBinary {
+    public static void main(String[] args) {
+        try(FileInputStream inputStream = new FileInputStream(".jpeg");
+            FileOutputStream outputStream = new FileOutputStream(".jpeg"))
+        {
+            byte[] chunk = new byte[1024];
+            int byteRead;
+            while((byteRead = inputStream.read(chunk)) !=-1)  {
+                outputStream.write(chunk, 0, byteRead);
+            }
+        } catch (IOException e) {
+            System.out.println("Error in reading or writting the file");
+        }
+    }
+}
