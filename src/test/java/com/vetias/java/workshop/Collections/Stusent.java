35fe1d915603c183ade2c5ca80e4485cfb4f23f5
pramisha <pramishaprami2@gmail.com>
2025-07-11 09:26:39 +0530

    java.student

---
+++ src/test/java/com/vetias/java/workshop/Collections/Stusent.java
@@ -0,0 +1,19 @@
+package com.vetias.java.workshop.Collections;
+
+public class Stusent {
+    public static void main(String[]args){
+        String[] [] students =
+    {
+        {"01","sowme"},{"02","arthi"},{"03","mitra"},{"04","karthi"}
+
+    };
+    System.out.println("roll number and name");
+    for(int i=0;i<students.length;i++)
+    {
+        System.out.printf(students[i][0],students[i][i]);
+
+    }
+    }
+    
+}
+    
